3e14d0c94a3a9583c1ad0da6ec866bb1f2f329e3
Felipe augusto <faugustogf@gmail.com>
2026-06-14 00:48:36 -0300

    Align country names with Jogos tab

--- src/data/worldcup2026.ts
+++ src/data/worldcup2026.ts
@@ -22,76 +22,76 @@ export interface Jogo {
 // 48 seleções da Copa do Mundo FIFA 2026 — editáveis manualmente.
 export const selecoes: Selecao[] = [
   // Grupo A
-  { id: "mex", nome: "México", grupo: "A", bandeiraEmoji: "🇲🇽", tecnico: "Javier Aguirre", jogadores: ["Guillermo Ochoa", "Edson Álvarez", "Hirving Lozano", "Raúl Jiménez", "Santiago Giménez"] },
-  { id: "rsa", nome: "África do Sul", grupo: "A", bandeiraEmoji: "🇿🇦", tecnico: "Hugo Broos", jogadores: ["Ronwen Williams", "Percy Tau", "Themba Zwane", "Teboho Mokoena", "Lyle Foster"] },
-  { id: "kor", nome: "Coreia do Sul", grupo: "A", bandeiraEmoji: "🇰🇷", tecnico: "Hong Myung-bo", jogadores: ["Son Heung-min", "Kim Min-jae", "Lee Kang-in", "Hwang Hee-chan", "Cho Gue-sung"] },
-  { id: "cze", nome: "República Tcheca", grupo: "A", bandeiraEmoji: "🇨🇿", tecnico: "Ivan Hašek", jogadores: ["Patrik Schick", "Tomáš Souček", "Vladimír Coufal", "Adam Hložek", "Lukáš Provod"] },
+  { id: "mex", nome: "Mexico", grupo: "A", bandeiraEmoji: "🇲🇽", tecnico: "Javier Aguirre", jogadores: ["Guillermo Ochoa", "Edson Álvarez", "Hirving Lozano", "Raúl Jiménez", "Santiago Giménez"] },
+  { id: "rsa", nome: "South Africa", grupo: "A", bandeiraEmoji: "🇿🇦", tecnico: "Hugo Broos", jogadores: ["Ronwen Williams", "Percy Tau", "Themba Zwane", "Teboho Mokoena", "Lyle Foster"] },
+  { id: "kor", nome: "South Korea", grupo: "A", bandeiraEmoji: "🇰🇷", tecnico: "Hong Myung-bo", jogadores: ["Son Heung-min", "Kim Min-jae", "Lee Kang-in", "Hwang Hee-chan", "Cho Gue-sung"] },
+  { id: "cze", nome: "Czech Republic", grupo: "A", bandeiraEmoji: "🇨🇿", tecnico: "Ivan Hašek", jogadores: ["Patrik Schick", "Tomáš Souček", "Vladimír Coufal", "Adam Hložek", "Lukáš Provod"] },
 
   // Grupo B
-  { id: "can", nome: "Canadá", grupo: "B", bandeiraEmoji: "🇨🇦", tecnico: "Jesse Marsch", jogadores: ["Alphonso Davies", "Jonathan David", "Stephen Eustáquio", "Tajon Buchanan", "Cyle Larin"] },
-  { id: "bih", nome: "Bósnia e Herzegovina", grupo: "B", bandeiraEmoji: "🇧🇦", tecnico: "Sergej Barbarez", jogadores: ["Edin Džeko", "Miralem Pjanić", "Sead Kolašinac", "Ermedin Demirović", "Amar Dedić"] },
-  { id: "qat", nome: "Catar", grupo: "B", bandeiraEmoji: "🇶🇦", tecnico: "Bartolomé Márquez", jogadores: ["Akram Afif", "Almoez Ali", "Hassan Al-Haydos", "Boualem Khoukhi", "Saad Al-Sheeb"] },
-  { id: "sui", nome: "Suíça", grupo: "B", bandeiraEmoji: "🇨🇭", tecnico: "Murat Yakin", jogadores: ["Granit Xhaka", "Manuel Akanji", "Breel Embolo", "Yann Sommer", "Xherdan Shaqiri"] },
+  { id: "can", nome: "Canada", grupo: "B", bandeiraEmoji: "🇨🇦", tecnico: "Jesse Marsch", jogadores: ["Alphonso Davies", "Jonathan David", "Stephen Eustáquio", "Tajon Buchanan", "Cyle Larin"] },
+  { id: "bih", nome: "Bosnia and Herzegovina", grupo: "B", bandeiraEmoji: "🇧🇦", tecnico: "Sergej Barbarez", jogadores: ["Edin Džeko", "Miralem Pjanić", "Sead Kolašinac", "Ermedin Demirović", "Amar Dedić"] },
+  { id: "qat", nome: "Qatar", grupo: "B", bandeiraEmoji: "🇶🇦", tecnico: "Bartolomé Márquez", jogadores: ["Akram Afif", "Almoez Ali", "Hassan Al-Haydos", "Boualem Khoukhi", "Saad Al-Sheeb"] },
+  { id: "sui", nome: "Switzerland", grupo: "B", bandeiraEmoji: "🇨🇭", tecnico: "Murat Yakin", jogadores: ["Granit Xhaka", "Manuel Akanji", "Breel Embolo", "Yann Sommer", "Xherdan Shaqiri"] },
 
   // Grupo C
-  { id: "bra", nome: "Brasil", grupo: "C", bandeiraEmoji: "🇧🇷", tecnico: "Carlo Ancelotti", jogadores: ["Alisson", "Marquinhos", "Vinícius Júnior", "Rodrygo", "Raphinha", "Neymar"] },
-  { id: "mar", nome: "Marrocos", grupo: "C", bandeiraEmoji: "🇲🇦", tecnico: "Walid Regragui", jogadores: ["Yassine Bounou", "Achraf Hakimi", "Hakim Ziyech", "Youssef En-Nesyri", "Brahim Díaz"] },
+  { id: "bra", nome: "Brazil", grupo: "C", bandeiraEmoji: "🇧🇷", tecnico: "Carlo Ancelotti", jogadores: ["Alisson", "Marquinhos", "Vinícius Júnior", "Rodrygo", "Raphinha", "Neymar"] },
+  { id: "mar", nome: "Morocco", grupo: "C", bandeiraEmoji: "🇲🇦", tecnico: "Walid Regragui", jogadores: ["Yassine Bounou", "Achraf Hakimi", "Hakim Ziyech", "Youssef En-Nesyri", "Brahim Díaz"] },
   { id: "hai", nome: "Haiti", grupo: "C", bandeiraEmoji: "🇭🇹", tecnico: "Sébastien Migné", jogadores: ["Duckens Nazon", "Frantzdy Pierrot", "Carl Sainté", "Ricardo Adé", "Johnny Placide"] },
-  { id: "sco", nome: "Escócia", grupo: "C", bandeiraEmoji: "🏴󠁧󠁢󠁳󠁣󠁴󠁿", tecnico: "Steve Clarke", jogadores: ["Andy Robertson", "Scott McTominay", "John McGinn", "Kieran Tierney", "Che Adams"] },
+  { id: "sco", nome: "Scotland", grupo: "C", bandeiraEmoji: "🏴󠁧󠁢󠁳󠁣󠁴󠁿", tecnico: "Steve Clarke", jogadores: ["Andy Robertson", "Scott McTominay", "John McGinn", "Kieran Tierney", "Che Adams"] },
 
   // Grupo D
-  { id: "usa", nome: "Estados Unidos", grupo: "D", bandeiraEmoji: "🇺🇸", tecnico: "Mauricio Pochettino", jogadores: ["Christian Pulisic", "Tyler Adams", "Weston McKennie", "Tim Weah", "Folarin Balogun"] },
-  { id: "par", nome: "Paraguai", grupo: "D", bandeiraEmoji: "🇵🇾", tecnico: "Gustavo Alfaro", jogadores: ["Gustavo Gómez", "Miguel Almirón", "Antonio Sanabria", "Julio Enciso", "Damián Bobadilla"] },
-  { id: "aus", nome: "Austrália", grupo: "D", bandeiraEmoji: "🇦🇺", tecnico: "Tony Popovic", jogadores: ["Mat Ryan", "Harry Souttar", "Aaron Mooy", "Mitchell Duke", "Riley McGree"] },
-  { id: "tur", nome: "Turquia", grupo: "D", bandeiraEmoji: "🇹🇷", tecnico: "Vincenzo Montella", jogadores: ["Hakan Çalhanoğlu", "Arda Güler", "Kenan Yıldız", "Merih Demiral", "Kerem Aktürkoğlu"] },
+  { id: "usa", nome: "United States", grupo: "D", bandeiraEmoji: "🇺🇸", tecnico: "Mauricio Pochettino", jogadores: ["Christian Pulisic", "Tyler Adams", "Weston McKennie", "Tim Weah", "Folarin Balogun"] },
+  { id: "par", nome: "Paraguay", grupo: "D", bandeiraEmoji: "🇵🇾", tecnico: "Gustavo Alfaro", jogadores: ["Gustavo Gómez", "Miguel Almirón", "Antonio Sanabria", "Julio Enciso", "Damián Bobadilla"] },
+  { id: "aus", nome: "Australia", grupo: "D", bandeiraEmoji: "🇦🇺", tecnico: "Tony Popovic", jogadores: ["Mat Ryan", "Harry Souttar", "Aaron Mooy", "Mitchell Duke", "Riley McGree"] },
+  { id: "tur", nome: "Turkey", grupo: "D", bandeiraEmoji: "🇹🇷", tecnico: "Vincenzo Montella", jogadores: ["Hakan Çalhanoğlu", "Arda Güler", "Kenan Yıldız", "Merih Demiral", "Kerem Aktürkoğlu"] },
 
   // Grupo E
-  { id: "ger", nome: "Alemanha", grupo: "E", bandeiraEmoji: "🇩🇪", tecnico: "Julian Nagelsmann", jogadores: ["Manuel Neuer", "Joshua Kimmich", "Jamal Musiala", "Florian Wirtz", "Kai Havertz"] },
-  { id: "cur", nome: "Curaçau", grupo: "E", bandeiraEmoji: "🇨🇼", tecnico: "Dick Advocaat", jogadores: ["Leandro Bacuna", "Juninho Bacuna", "Eloy Room", "Cuco Martina", "Tahith Chong"] },
-  { id: "civ", nome: "Costa do Marfim", grupo: "E", bandeiraEmoji: "🇨🇮", tecnico: "Emerse Faé", jogadores: ["Franck Kessié", "Sébastien Haller", "Simon Adingra", "Évan Ndicka", "Nicolas Pépé"] },
-  { id: "ecu", nome: "Equador", grupo: "E", bandeiraEmoji: "🇪🇨", tecnico: "Sebastián Beccacece", jogadores: ["Moisés Caicedo", "Enner Valencia", "Pervis Estupiñán", "Piero Hincapié", "Kendry Páez"] },
+  { id: "ger", nome: "Germany", grupo: "E", bandeiraEmoji: "🇩🇪", tecnico: "Julian Nagelsmann", jogadores: ["Manuel Neuer", "Joshua Kimmich", "Jamal Musiala", "Florian Wirtz", "Kai Havertz"] },
+  { id: "cur", nome: "Curaçao", grupo: "E", bandeiraEmoji: "🇨🇼", tecnico: "Dick Advocaat", jogadores: ["Leandro Bacuna", "Juninho Bacuna", "Eloy Room", "Cuco Martina", "Tahith Chong"] },
+  { id: "civ", nome: "Ivory Coast", grupo: "E", bandeiraEmoji: "🇨🇮", tecnico: "Emerse Faé", jogadores: ["Franck Kessié", "Sébastien Haller", "Simon Adingra", "Évan Ndicka", "Nicolas Pépé"] },
+  { id: "ecu", nome: "Ecuador", grupo: "E", bandeiraEmoji: "🇪🇨", tecnico: "Sebastián Beccacece", jogadores: ["Moisés Caicedo", "Enner Valencia", "Pervis Estupiñán", "Piero Hincapié", "Kendry Páez"] },
 
   // Grupo F
-  { id: "ned", nome: "Holanda", grupo: "F", bandeiraEmoji: "🇳🇱", tecnico: "Ronald Koeman", jogadores: ["Virgil van Dijk", "Frenkie de Jong", "Memphis Depay", "Cody Gakpo", "Xavi Simons"] },
-  { id: "jpn", nome: "Japão", grupo: "F", bandeiraEmoji: "🇯🇵", tecnico: "Hajime Moriyasu", jogadores: ["Takefusa Kubo", "Wataru Endo", "Kaoru Mitoma", "Daichi Kamada", "Ayase Ueda"] },
-  { id: "swe", nome: "Suécia", grupo: "F", bandeiraEmoji: "🇸🇪", tecnico: "Graham Potter", jogadores: ["Alexander Isak", "Viktor Gyökeres", "Dejan Kulusevski", "Lucas Bergvall", "Anthony Elanga"] },
-  { id: "tun", nome: "Tunísia", grupo: "F", bandeiraEmoji: "🇹🇳", tecnico: "Sami Trabelsi", jogadores: ["Aïssa Laïdouni", "Hannibal Mejbri", "Wahbi Khazri", "Youssef Msakni", "Ellyes Skhiri"] },
+  { id: "ned", nome: "Netherlands", grupo: "F", bandeiraEmoji: "🇳🇱", tecnico: "Ronald Koeman", jogadores: ["Virgil van Dijk", "Frenkie de Jong", "Memphis Depay", "Cody Gakpo", "Xavi Simons"] },
+  { id: "jpn", nome: "Japan", grupo: "F", bandeiraEmoji: "🇯🇵", tecnico: "Hajime Moriyasu", jogadores: ["Takefusa Kubo", "Wataru Endo", "Kaoru Mitoma", "Daichi Kamada", "Ayase Ueda"] },
+  { id: "swe", nome: "Sweden", grupo: "F", bandeiraEmoji: "🇸🇪", tecnico: "Graham Potter", jogadores: ["Alexander Isak", "Viktor Gyökeres", "Dejan Kulusevski", "Lucas Bergvall", "Anthony Elanga"] },
+  { id: "tun", nome: "Tunisia", grupo: "F", bandeiraEmoji: "🇹🇳", tecnico: "Sami Trabelsi", jogadores: ["Aïssa Laïdouni", "Hannibal Mejbri", "Wahbi Khazri", "Youssef Msakni", "Ellyes Skhiri"] },
 
   // Grupo G
-  { id: "bel", nome: "Bélgica", grupo: "G", bandeiraEmoji: "🇧🇪", tecnico: "Rudi Garcia", jogadores: ["Kevin De Bruyne", "Romelu Lukaku", "Thibaut Courtois", "Jérémy Doku", "Youri Tielemans"] },
-  { id: "egy", nome: "Egito", grupo: "G", bandeiraEmoji: "🇪🇬", tecnico: "Hossam Hassan", jogadores: ["Mohamed Salah", "Mohamed Elneny", "Trezeguet", "Omar Marmoush", "Mostafa Mohamed"] },
-  { id: "irn", nome: "Irã", grupo: "G", bandeiraEmoji: "🇮🇷", tecnico: "Amir Ghalenoei", jogadores: ["Mehdi Taremi", "Sardar Azmoun", "Alireza Jahanbakhsh", "Saman Ghoddos", "Alireza Beiranvand"] },
-  { id: "nzl", nome: "Nova Zelândia", grupo: "G", bandeiraEmoji: "🇳🇿", tecnico: "Darren Bazeley", jogadores: ["Chris Wood", "Marko Stamenić", "Liberato Cacace", "Kosta Barbarouses", "Elijah Just"] },
+  { id: "bel", nome: "Belgium", grupo: "G", bandeiraEmoji: "🇧🇪", tecnico: "Rudi Garcia", jogadores: ["Kevin De Bruyne", "Romelu Lukaku", "Thibaut Courtois", "Jérémy Doku", "Youri Tielemans"] },
+  { id: "egy", nome: "Egypt", grupo: "G", bandeiraEmoji: "🇪🇬", tecnico: "Hossam Hassan", jogadores: ["Mohamed Salah", "Mohamed Elneny", "Trezeguet", "Omar Marmoush", "Mostafa Mohamed"] },
+  { id: "irn", nome: "Iran", grupo: "G", bandeiraEmoji: "🇮🇷", tecnico: "Amir Ghalenoei", jogadores: ["Mehdi Taremi", "Sardar Azmoun", "Alireza Jahanbakhsh", "Saman Ghoddos", "Alireza Beiranvand"] },
+  { id: "nzl", nome: "New Zealand", grupo: "G", bandeiraEmoji: "🇳🇿", tecnico: "Darren Bazeley", jogadores: ["Chris Wood", "Marko Stamenić", "Liberato Cacace", "Kosta Barbarouses", "Elijah Just"] },
 
   // Grupo H
-  { id: "esp", nome: "Espanha", grupo: "H", bandeiraEmoji: "🇪🇸", tecnico: "Luis de la Fuente", jogadores: ["Lamine Yamal", "Pedri", "Rodri", "Nico Williams", "Álvaro Morata"] },
-  { id: "cpv", nome: "Cabo Verde", grupo: "H", bandeiraEmoji: "🇨🇻", tecnico: "Bubista", jogadores: ["Ryan Mendes", "Vozinha", "Stopira", "Garry Rodrigues", "Bebé"] },
-  { id: "ksa", nome: "Arábia Saudita", grupo: "H", bandeiraEmoji: "🇸🇦", tecnico: "Hervé Renard", jogadores: ["Salem Al-Dawsari", "Saleh Al-Shehri", "Mohammed Kanno", "Salman Al-Faraj", "Sultan Al-Ghannam"] },
-  { id: "uru", nome: "Uruguai", grupo: "H", bandeiraEmoji: "🇺🇾", tecnico: "Marcelo Bielsa", jogadores: ["Federico Valverde", "Darwin Núñez", "Ronald Araújo", "Giorgian de Arrascaeta", "Maximiliano Araújo"] },
+  { id: "esp", nome: "Spain", grupo: "H", bandeiraEmoji: "🇪🇸", tecnico: "Luis de la Fuente", jogadores: ["Lamine Yamal", "Pedri", "Rodri", "Nico Williams", "Álvaro Morata"] },
+  { id: "cpv", nome: "Cape Verde", grupo: "H", bandeiraEmoji: "🇨🇻", tecnico: "Bubista", jogadores: ["Ryan Mendes", "Vozinha", "Stopira", "Garry Rodrigues", "Bebé"] },
+  { id: "ksa", nome: "Saudi Arabia", grupo: "H", bandeiraEmoji: "🇸🇦", tecnico: "Hervé Renard", jogadores: ["Salem Al-Dawsari", "Saleh Al-Shehri", "Mohammed Kanno", "Salman Al-Faraj", "Sultan Al-Ghannam"] },
+  { id: "uru", nome: "Uruguay", grupo: "H", bandeiraEmoji: "🇺🇾", tecnico: "Marcelo Bielsa", jogadores: ["Federico Valverde", "Darwin Núñez", "Ronald Araújo", "Giorgian de Arrascaeta", "Maximiliano Araújo"] },
 
   // Grupo I
-  { id: "fra", nome: "França", grupo: "I", bandeiraEmoji: "🇫🇷", tecnico: "Didier Deschamps", jogadores: ["Kylian Mbappé", "Antoine Griezmann", "Aurélien Tchouaméni", "William Saliba", "Ousmane Dembélé"] },
+  { id: "fra", nome: "France", grupo: "I", bandeiraEmoji: "🇫🇷", tecnico: "Didier Deschamps", jogadores: ["Kylian Mbappé", "Antoine Griezmann", "Aurélien Tchouaméni", "William Saliba", "Ousmane Dembélé"] },
   { id: "sen", nome: "Senegal", grupo: "I", bandeiraEmoji: "🇸🇳", tecnico: "Pape Thiaw", jogadores: ["Sadio Mané", "Kalidou Koulibaly", "Édouard Mendy", "Ismaïla Sarr", "Nicolas Jackson"] },
-  { id: "irq", nome: "Iraque", grupo: "I", bandeiraEmoji: "🇮🇶", tecnico: "Graham Arnold", jogadores: ["Aymen Hussein", "Ali Al-Hamadi", "Zidane Iqbal", "Ibrahim Bayesh", "Jalal Hassan"] },
-  { id: "nor", nome: "Noruega", grupo: "I", bandeiraEmoji: "🇳🇴", tecnico: "Ståle Solbakken", jogadores: ["Erling Haaland", "Martin Ødegaard", "Alexander Sørloth", "Antonio Nusa", "Oscar Bobb"] },
+  { id: "irq", nome: "Iraq", grupo: "I", bandeiraEmoji: "🇮🇶", tecnico: "Graham Arnold", jogadores: ["Aymen Hussein", "Ali Al-Hamadi", "Zidane Iqbal", "Ibrahim Bayesh", "Jalal Hassan"] },
+  { id: "nor", nome: "Norway", grupo: "I", bandeiraEmoji: "🇳🇴", tecnico: "Ståle Solbakken", jogadores: ["Erling Haaland", "Martin Ødegaard", "Alexander Sørloth", "Antonio Nusa", "Oscar Bobb"] },
 
   // Grupo J
-  { id: "aut", nome: "Áustria", grupo: "J", bandeiraEmoji: "🇦🇹", tecnico: "Ralf Rangnick", jogadores: ["David Alaba", "Marcel Sabitzer", "Marko Arnautović", "Konrad Laimer", "Christoph Baumgartner"] },
-  { id: "jor", nome: "Jordânia", grupo: "J", bandeiraEmoji: "🇯🇴", tecnico: "Jamal Sellami", jogadores: ["Musa Al-Taamari", "Yazan Al-Naimat", "Mahmoud Al-Mardi", "Nour Al-Rawabdeh", "Ehsan Haddad"] },
+  { id: "aut", nome: "Austria", grupo: "J", bandeiraEmoji: "🇦🇹", tecnico: "Ralf Rangnick", jogadores: ["David Alaba", "Marcel Sabitzer", "Marko Arnautović", "Konrad Laimer", "Christoph Baumgartner"] },
+  { id: "jor", nome: "Jordan", grupo: "J", bandeiraEmoji: "🇯🇴", tecnico: "Jamal Sellami", jogadores: ["Musa Al-Taamari", "Yazan Al-Naimat", "Mahmoud Al-Mardi", "Nour Al-Rawabdeh", "Ehsan Haddad"] },
   { id: "arg", nome: "Argentina", grupo: "J", bandeiraEmoji: "🇦🇷", tecnico: "Lionel Scaloni", jogadores: ["Lionel Messi", "Emiliano Martínez", "Julián Álvarez", "Enzo Fernández", "Lautaro Martínez"] },
-  { id: "alg", nome: "Argélia", grupo: "J", bandeiraEmoji: "🇩🇿", tecnico: "Vladimir Petković", jogadores: ["Riyad Mahrez", "Ismaël Bennacer", "Saïd Benrahma", "Houssem Aouar", "Baghdad Bounedjah"] },
+  { id: "alg", nome: "Algeria", grupo: "J", bandeiraEmoji: "🇩🇿", tecnico: "Vladimir Petković", jogadores: ["Riyad Mahrez", "Ismaël Bennacer", "Saïd Benrahma", "Houssem Aouar", "Baghdad Bounedjah"] },
 
   // Grupo K
   { id: "por", nome: "Portugal", grupo: "K", bandeiraEmoji: "🇵🇹", tecnico: "Roberto Martínez", jogadores: ["Cristiano Ronaldo", "Bruno Fernandes", "Bernardo Silva", "Rúben Dias", "Rafael Leão"] },
-  { id: "cod", nome: "Rep. Dem. do Congo", grupo: "K", bandeiraEmoji: "🇨🇩", tecnico: "Sébastien Desabre", jogadores: ["Cédric Bakambu", "Chancel Mbemba", "Yoane Wissa", "Théo Bongonda", "Arthur Masuaku"] },
-  { id: "uzb", nome: "Uzbequistão", grupo: "K", bandeiraEmoji: "🇺🇿", tecnico: "Timur Kapadze", jogadores: ["Eldor Shomurodov", "Abbosbek Fayzullaev", "Otabek Shukurov", "Khojimat Erkinov", "Jasur Yakhshiboev"] },
-  { id: "col", nome: "Colômbia", grupo: "K", bandeiraEmoji: "🇨🇴", tecnico: "Néstor Lorenzo", jogadores: ["James Rodríguez", "Luis Díaz", "Jhon Durán", "Davinson Sánchez", "Richard Ríos"] },
+  { id: "cod", nome: "DR Congo", grupo: "K", bandeiraEmoji: "🇨🇩", tecnico: "Sébastien Desabre", jogadores: ["Cédric Bakambu", "Chancel Mbemba", "Yoane Wissa", "Théo Bongonda", "Arthur Masuaku"] },
+  { id: "uzb", nome: "Uzbekistan", grupo: "K", bandeiraEmoji: "🇺🇿", tecnico: "Timur Kapadze", jogadores: ["Eldor Shomurodov", "Abbosbek Fayzullaev", "Otabek Shukurov", "Khojimat Erkinov", "Jasur Yakhshiboev"] },
+  { id: "col", nome: "Colombia", grupo: "K", bandeiraEmoji: "🇨🇴", tecnico: "Néstor Lorenzo", jogadores: ["James Rodríguez", "Luis Díaz", "Jhon Durán", "Davinson Sánchez", "Richard Ríos"] },
 
   // Grupo L
-  { id: "eng", nome: "Inglaterra", grupo: "L", bandeiraEmoji: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", tecnico: "Thomas Tuchel", jogadores: ["Harry Kane", "Jude Bellingham", "Bukayo Saka", "Phil Foden", "Declan Rice"] },
-  { id: "cro", nome: "Croácia", grupo: "L", bandeiraEmoji: "🇭🇷", tecnico: "Zlatko Dalić", jogadores: ["Luka Modrić", "Mateo Kovačić", "Andrej Kramarić", "Joško Gvardiol", "Mario Pašalić"] },
-  { id: "gha", nome: "Gana", grupo: "L", bandeiraEmoji: "🇬🇭", tecnico: "Otto Addo", jogadores: ["Mohammed Kudus", "Thomas Partey", "Iñaki Williams", "Jordan Ayew", "Antoine Semenyo"] },
-  { id: "pan", nome: "Panamá", grupo: "L", bandeiraEmoji: "🇵🇦", tecnico: "Thomas Christiansen", jogadores: ["Aníbal Godoy", "Adalberto Carrasquilla", "José Fajardo", "Michael Murillo", "Cecilio Waterman"] },
+  { id: "eng", nome: "England", grupo: "L", bandeiraEmoji: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", tecnico: "Thomas Tuchel", jogadores: ["Harry Kane", "Jude Bellingham", "Bukayo Saka", "Phil Foden", "Declan Rice"] },
+  { id: "cro", nome: "Croatia", grupo: "L", bandeiraEmoji: "🇭🇷", tecnico: "Zlatko Dalić", jogadores: ["Luka Modrić", "Mateo Kovačić", "Andrej Kramarić", "Joško Gvardiol", "Mario Pašalić"] },
+  { id: "gha", nome: "Ghana", grupo: "L", bandeiraEmoji: "🇬🇭", tecnico: "Otto Addo", jogadores: ["Mohammed Kudus", "Thomas Partey", "Iñaki Williams", "Jordan Ayew", "Antoine Semenyo"] },
+  { id: "pan", nome: "Panama", grupo: "L", bandeiraEmoji: "🇵🇦", tecnico: "Thomas Christiansen", jogadores: ["Aníbal Godoy", "Adalberto Carrasquilla", "José Fajardo", "Michael Murillo", "Cecilio Waterman"] },
 ];
 
 // Estádios oficiais por cidade-sede.
